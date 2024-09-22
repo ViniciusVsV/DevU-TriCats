45af5cf171acfb9e7d681a377403f4ac63279246
ViniciusVsV <viniciusvsv193@gmail.com>
2024-09-22 11:28:51 -0300

    Seletor de fases + playerprefs para armazenar quais fases ja foram jogadas + caixas dinamicas

---
+++ Assets/Scripts/PhaseSelector/BoxReact.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BoxReact : MonoBehaviour{
+    [SerializeField] private Sprite hoveredSprite;
+    [SerializeField] private Sprite defaultSprite;
+    [SerializeField] private string levelName;
+
+    private SpriteRenderer spriteRenderer;
+    public bool isHovered;
+    public bool isDisabled;
+
+    void Start(){
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = defaultSprite;
+
+        isHovered = false;
+
+        if(PlayerPrefs.GetInt(levelName) == 1){
+            isDisabled = true;
+            spriteRenderer.color = Color.gray;
+        }
+        else{
+            isDisabled = false;
+            PlayerPrefs.SetInt(levelName, 0);
+        }
+    }
+
+    void Update(){
+        if(isHovered && Input.GetMouseButtonDown(0) && PlayerPrefs.GetInt(levelName) == 0){
+            PlayerPrefs.SetInt(levelName, 1);
+            SceneManager.LoadScene(levelName);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other) {
+        if(other.CompareTag("CatSelector")){
+            if(!isDisabled){
+                spriteRenderer.sprite = hoveredSprite;
+                isHovered = true;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if(other.CompareTag("CatSelector")){
+            spriteRenderer.sprite = defaultSprite;
+            isHovered = false;
+        }
+    }
+}
