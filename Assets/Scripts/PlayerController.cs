b0d49133d4916d9a60bf5869fc8a2aaafaed618e
ViniciusVsV <viniciusvsv193@gmail.com>
2024-09-22 18:49:41 -0300

    criando as animações dos gatos

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Animations;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour{
     [SerializeField] private float moveSpeed;
     private GameEndController gameEndController;
+    private Animator animator;
     private bool canMove;
 
     private Rigidbody2D rb;
@@ -16,6 +18,7 @@ public class PlayerController : MonoBehaviour{
     void Start(){
         rb = GetComponent<Rigidbody2D>();
         gameEndController = FindFirstObjectByType<GameEndController>();
+        animator = GetComponent<Animator>();
 
     }
 
@@ -29,6 +32,21 @@ public class PlayerController : MonoBehaviour{
 
         Vector2 moveDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
         rb.velocity = moveDirection * moveSpeed;
+
+        if(rb.velocity.x != 0f)
+            animator.SetBool("isMovingX", true);
+        else
+            animator.SetBool("isMovingX", false);
+
+        if(rb.velocity.y > Mathf.Epsilon)
+            animator.SetBool("isMovingUp", true);
+        else
+            animator.SetBool("isMovingUp", false);
+
+        if(rb.velocity.y < Mathf.Epsilon)
+            animator.SetBool("isMovingDown", true);
+        else
+            animator.SetBool("isMovingDown", false);
     }
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.CompareTag("Portal")){
